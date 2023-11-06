5bc3658b94f9f11af28cc1835f37b2b8dea091af
Cesar Herrera <cesarherr321@gmail.com>
2023-11-06 18:06:52 -0300

    Add postReserve extra reducer to reservationSlice

--- src/redux/reserves/reserveSlice.js
+++ src/redux/reserves/reserveSlice.js
@@ -49,7 +49,25 @@ const reserveSlice = createSlice({
         state.isLoading = true;
         state.error = undefined;
       })
+      .addCase(fetchItems.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      })
+      .addCase(postReserve.fulfilled, (state) => {
+        state.isLoading = false;
+        state.error = undefined;
+      })
+      .addCase(postReserve.pending, (state) => {
+        state.isLoading = true;
+        state.error = undefined;
+      })
+      .addCase(postReserve.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+        state.status = action.payload.error;
+      });
   },
 });
 
 export default reserveSlice.reducer;
+export { fetchReserves, deleteReserve };
