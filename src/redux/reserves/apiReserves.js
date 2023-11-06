549dc5bd48ae7a4ca165e4bc284eef97ee99e9de
Cesar Herrera <cesarherr321@gmail.com>
2023-11-06 17:53:04 -0300

    Add Api reserves file

---
+++ src/redux/reserves/apiReserves.js
@@ -0,0 +1,41 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const idUser = '1';
+
+const urlReserves = 'http://127.0.0.1:4000/api/v1/users/';
+
+const fetchReserves = createAsyncThunk('reserves/fetchReserves', async () => {
+  const response = await axios.get(`${urlReserves}${idUser}/appointments`);
+  return response.data;
+});
+
+const deleteReserve = createAsyncThunk('reserves/deleteReserve', async (ids) => {
+  const response = await axios.delete(`${urlReserves}${ids}`);
+  return response.data;
+});
+
+const postReserve = createAsyncThunk('reserves/postReserve', async (dataObject, { rejectWithValue }) => {
+  try {
+    const options = {
+      method: 'POST',
+      url: `${urlReserves}${idUser}/appointments`,
+      data: dataObject,
+    };
+
+    const response = await axios.request(options);
+    console.log(response.status);
+    return response.status;
+  } catch (error) {
+    return rejectWithValue(error.response.data);
+  }
+});
+
+const fetchItems = createAsyncThunk('items/fetchItems', async () => {
+  const response = await axios.get('http://127.0.0.1:4000/api/v1/items');
+  return response.data;
+});
+
+export {
+  fetchReserves, deleteReserve, postReserve, fetchItems,
+};
