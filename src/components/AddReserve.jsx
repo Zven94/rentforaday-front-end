05cd68ad0829f396ca798ea794ada6f229baed4f
Cesar Herrera <cesarherr321@gmail.com>
2023-11-06 18:37:10 -0300

    Add fetch items and items detail on addReserve Component

--- src/components/AddReserve.jsx
+++ src/components/AddReserve.jsx
@@ -1,6 +1,41 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchItems } from '../redux/reserves/apiReserves';
+import { setItemDetail } from '../redux/reserves/reserveSlice';
+import '../styles/addReserve.css';
+
 function AddReserve() {
+  const currentUser = 'user';
+  const dispatch = useDispatch();
+  let showSelect;
+  let itemContent;
+
+  const { selectedItem } = useSelector((state) => state.reserves);
+
+  // fetch items and reserves
+  useEffect(() => {
+    dispatch(fetchItems());
+    dispatch(setItemDetail(selectedItem));
+  }, [dispatch, selectedItem]);
+
   return (
-    <h1>Add reserve</h1>
+    <>
+      {currentUser.length > 0
+        ? (
+          <section className="addReserve">
+            <h1>Add a Reserve</h1>
+            <form>
+              {showSelect}
+
+              <button className="btn " type="button">Reserve Now</button>
+            </form>
+            <div className="itemContent container-sm text-center">
+              {itemContent}
+            </div>
+          </section>
+        )
+        : <h1 className="container-sm text-center">Sign in!! 👈</h1>}
+    </>
   );
 }
 
