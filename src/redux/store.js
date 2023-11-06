20dfbb853380b49d9d602b62679b5a88c95140c7
Ruthmy <ruthmy17@gmail.com>
2023-11-05 18:16:04 -0600

    Modified 'store.js' -redux store- - Imported itemReducer to handle item data. - Imported authReducer tu handle user session.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import itemReducer from './items/itemSlice';
+import authReducer from './users/authSlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    item: itemReducer,
+    auth: authReducer,
+  },
 });
 
 export default store;
