8e8be890ccfdf4bc6a0b3f95af344c30c6b67967
Ruthmy <ruthmy17@gmail.com>
2023-11-07 09:18:06 -0600

    Modified 'authSlice.js' -redux slice- - Created the fully functional version of the registration process.

--- src/redux/users/authSlice.js
+++ src/redux/users/authSlice.js
@@ -1,4 +1,7 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { toast } from 'react-hot-toast';
+import itemAPI from '../../API/itemAPI';
 
 const initialState = {
   token: null,
@@ -8,39 +11,110 @@ const initialState = {
   error: null,
 };
 
+// Async thunk for user login
+export const loginUser = createAsyncThunk(
+  'auth/loginUser',
+  async (formData, thunkAPI) => {
+    try {
+      const response = await axios.post(
+        `${itemAPI.baseURL}${itemAPI.login}`,
+        { user: formData },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+      // save the response data
+      const data = await response.data;
+
+      if (response.status === 200) {
+        // destructure the response data to get the token and user
+        const token = response.headers.authorization;
+        const { user } = data;
+        // save the token and user in localStorage
+        localStorage.setItem('token', token);
+        localStorage.setItem('user', JSON.stringify(user));
+        // success message to the user
+        toast.success(`Welcome, ${user.name}`);
+      }
+
+      // Return the user data
+      return data;
+    } catch (error) {
+      // Return the error message
+      toast.error(`Login failed. ${error.message}`);
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  },
+
+);
+
+// Async Thunk for user registration
+export const registerUser = createAsyncThunk(
+  'auth/registerUser',
+  async (formData, thunkAPI) => {
+    try {
+      const response = await axios.post(
+        `${itemAPI.baseURL}${itemAPI.registration}`,
+        { user: formData },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+      toast.success('Registration successful. Please login.');
+      return response.data;
+    } catch (error) {
+      toast.error(`Registration failed. ${error.message}`);
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  },
+);
+
 const authSlice = createSlice({
   name: 'auth',
   initialState,
-  reducers: {
-    authLoading(state) {
+  reducers: {},
+  extraReducers(builder) {
+    // registerUser
+    builder.addCase(registerUser.pending, (state) => {
       state.isLoading = true;
-    },
-    authSuccess(state, action) {
+    });
+    builder.addCase(registerUser.fulfilled, (state) => {
+      state.isLoading = false;
+      state.error = null;
+    });
+    builder.addCase(registerUser.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    });
+    // loginUser
+    builder.addCase(loginUser.pending, (state) => {
+      state.isLoading = true;
+    });
+    // en un escenario exitoso, guardar el token en el local storage
+    builder.addCase(loginUser.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.error = null;
       state.token = action.payload.token;
       state.user = action.payload.user;
       state.isAuthenticated = true;
-      state.isLoading = false;
-      state.error = null;
-    },
-    authFail(state, action) {
+    });
+    // en un escenario de error, mostrar el error
+    builder.addCase(loginUser.rejected, (state, action) => {
       state.isLoading = false;
       state.error = action.payload;
-    },
-    logout(state) {
-      state.token = null;
-      state.user = null;
-      state.isAuthenticated = false;
-      state.isLoading = false;
-      state.error = null;
-    },
+    });
   },
 });
 
-export const {
-  authLoading,
-  authSuccess,
-  authFail,
-  logout,
-} = authSlice.actions;
+// export const {
+//   authLoading,
+//   authSuccess,
+//   authFail,
+//   logout,
+// } = authSlice.actions;
 
 export default authSlice.reducer;
