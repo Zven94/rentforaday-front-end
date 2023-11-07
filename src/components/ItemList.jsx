01e2898eb5076cd03e99044e82111915f327588b
Ruthmy <ruthmy17@gmail.com>
2023-11-07 17:27:29 -0600

    Created 'ItemList.jsx' -component- - Empty component for the item list.

---
+++ src/components/ItemList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import { useSelector } from 'react-redux';
+// import { Link } from 'react-router-dom';
+// import './item.css';
+
+const Item = () => {
+
+};
+
+export default Item;
