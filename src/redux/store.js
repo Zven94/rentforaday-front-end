3a96192907117951cdea2865523ff1dd6abf5ffc
Ruthmy <ruthmy17@gmail.com>
2023-11-07 14:43:03 -0600

    Modified 'store.js' -redux store- - Added the tokenSlice.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import itemReducer from './items/itemSlice';
 import authReducer from './users/authSlice';
+import tokenReducer from './users/tokenSlice';
 
 const store = configureStore({
   reducer: {
     item: itemReducer,
     auth: authReducer,
+    token: tokenReducer,
   },
 });
 
