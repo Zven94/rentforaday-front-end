35d8a5ba1b3539c95135caf9534a776ee541de93
Ruthmy <ruthmy17@gmail.com>
2023-11-07 14:43:57 -0600

    Modified 'index.js' -component- - Removed the dispatch that initialy provides the store.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,8 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 
 import App from './App';
-import { fetchItems } from './redux/items/itemSlice';
 
 import './index.css';
-
-store.dispatch(fetchItems());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
