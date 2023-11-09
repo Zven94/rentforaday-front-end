b6da45e45e40862034641ab673575bd5bb7302da
Cesar Herrera <cesarherr321@gmail.com>
2023-11-09 02:53:01 -0300

    Refactor logic to get images

--- src/API/itemAPI.js
+++ src/API/itemAPI.js
@@ -1,14 +1,15 @@
 // Purpose: RentForADay Item API base URL and endpoints
 
 const itemAPI = {
-  // baseURL: 'http://127.0.0.1:4000/',
-  baseURL: 'https://rent-for-a-day.onrender.com/',
+  baseURL: 'http://127.0.0.1:4000/',
+  // baseURL: 'https://rent-for-a-day.onrender.com/',
   currentUser: 'current_user',
   login: 'login',
   logout: 'logout',
   registration: 'api/v1/users',
   allUsers: 'api/v1/users',
   listItems: 'api/v1/items',
+  listReserves: 'api/v1/users/',
 };
 
 export default itemAPI;
