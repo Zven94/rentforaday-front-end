5766166edf15fa2275e8a37bc324eca2c1b36a61
Ruthmy <ruthmy17@gmail.com>
2023-11-09 19:50:38 -0600

    Modified 'root.js' -view- - Updated syntax to ES6.

--- src/routes/root.js
+++ src/routes/root.js
@@ -1,13 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../components/SideBar';
 
-function Root() {
-  return (
-    <section className="nav">
-      <Sidebar />
-      <Outlet />
-    </section>
-  );
-}
+const Root = () => (
+  <section className="nav">
+    <Sidebar />
+    <Outlet />
+  </section>
+);
 
 export default Root;
